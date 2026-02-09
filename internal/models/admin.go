45538be14e18db482076fc0e6d63a12991c94756
Atikur Rahman Rajib <atikurajib@gmail.com>
2026-02-09 09:16:42 +0600

    Update admin.go

--- internal/models/admin.go
+++ internal/models/admin.go
@@ -1,1 +1,13 @@
+package models
+
+import "time"
+
+type Admin struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
