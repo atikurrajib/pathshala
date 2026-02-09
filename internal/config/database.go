7c7d683312c9ceba702ac9a7d5feb6ccf9014217
Atikur Rahman Rajib <atikurajib@gmail.com>
2026-02-09 09:14:00 +0600

    Update database.go

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,1 +1,52 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var DB *pgxpool.Pool
+
+func ConnectDB() {
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		log.Fatalf("Unable to parse database URL: %v", err)
+	}
+
+	config.MaxConns = 10
+	config.MinConns = 2
+	config.MaxConnLifetime = 1 * time.Hour
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+
+	DB = pool
+	fmt.Println("Connected to PostgreSQL successfully!")
+}
+
+func CloseDB() {
+	if DB != nil {
+		DB.Close()
+		fmt.Println("Database connection closed.")
+	}
+}
 
