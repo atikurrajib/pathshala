408e92ccb5a25211a397e67202ef32c8488f1e15
FardinTheCoder <hasan2305341393@diu.edu.bd>
2026-02-09 09:06:07 +0600

    Update 000010_indices.up.sql

--- migrations/000010_indices.up.sql
+++ migrations/000010_indices.up.sql
@@ -1,3 +1,12 @@
-CREATE INDEX idx_students_email ON students(email);
-CREATE INDEX idx_teachers_email ON teachers(email);
-CREATE INDEX idx_course_code ON courses(course_code);
+CREATE INDEX idx_students_dept ON students(department_id);
+CREATE INDEX idx_teachers_dept ON teachers(department_id);
+CREATE INDEX idx_courses_dept ON courses(department_id);
+
+CREATE INDEX idx_enrollments_student ON enrollments(student_id);
+CREATE INDEX idx_enrollments_course ON enrollments(course_id);
+
+CREATE INDEX idx_exam_marks_student ON exam_marks(student_id);
+CREATE INDEX idx_attendance_student ON attendance(student_id);
+CREATE INDEX idx_attendance_date ON attendance(date);
+
+CREATE INDEX idx_students_name ON students(name);
